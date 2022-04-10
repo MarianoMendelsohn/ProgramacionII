940269be1850e5cd4771df19f55d569671fd3030
kundo14 <facundo.tabanera@gmail.com>
2022-04-10 17:28:21 -0300

    Corrección de Datos
    Datos en carpeta db

--- myApp/data/db/usuarios.js
+++ myApp/data/db/usuarios.js
@@ -2,50 +2,49 @@ let usuarios = [{
 
     nombreDeUsuario: 'Facundo',
     email:'kundo@gmail.com',
-    contraseña: 'hola123'
+    contraseña: 'hola123',
     fechaDeNac: 06/03/99,
     dni:41781311, 
-    fotoDePerfil: 
-};
+    fotoDePerfil: "",
+},
 
 {
     nombreDeUsuario: 'Mariano',
     email: 'mariano@gmail.com',
     contraseña:'hola123',
     fechaDeNac: 03/09/99,
-    dni: 41888222
-    fotoDePerfil:
+    dni: 41888222,
+    fotoDePerfil: "",
 
-};
+},
 {
 
     nombreDeUsuario: 'Franco',
     email: 'franco@gmail.com',
-    contraseña: 'hola123'
-    fechaDeNac: 09/09/99
-    dni:41758902
-    fotoDePerfil:
-};
+    contraseña: 'hola123',
+    fechaDeNac: 09/09/99,
+    dni:41758902,
+    fotoDePerfil:"",
+},
 {
 
     nombreDeUsuario: 'Mauro',
     email: 'mauro@gmail.com', 
-    contraseña:'hola123'
+    contraseña:'hola123',
     fechaDeNac: 08/4/99,
-    dni: 41887921
-    fotoDePerfil:
-};
+    dni: 41887921,
+    fotoDePerfil: "",
+},
 {
 
     nombreDeUsuario: 'Brian',
     email: 'brian@gmail.com',
     contraseña: 'hola123',
     fechaDeNac: 09/09/90,
-    dni: 34898321
-    fotoDePerfil:
-};
+    dni: 34898321,
+    fotoDePerfil:"",
+},
 ]
 
 
    
-]
