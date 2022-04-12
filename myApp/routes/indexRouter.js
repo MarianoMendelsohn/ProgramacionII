d48e3f85233770b500d70616df2c061c1f37a5e2
kundo14 <facundo.tabanera@gmail.com>
2022-04-12 19:50:33 -0300

    CSS integrado
    Gracias Brian

--- myApp/routes/indexRouter.js
+++ myApp/routes/indexRouter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const indexController =require('../controllers/indexController') 
 
-router.get('/index', indexController.index); // router.get marca el suffijo
+router.get('/', indexController.index); // router.get marca el suffijo
 //router.get('/detalle/:id')
 
 module.exports = router;
