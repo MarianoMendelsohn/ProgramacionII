ff0aa319d760b2a7c4b39e7817abefe5b82d3695
kundo14 <facundo.tabanera@gmail.com>
2022-05-23 09:59:47 -0300

    Create data.sql

---
+++ myApp/data/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE usuario {
+
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT 
+nombre VARCHAR(50) NOT NULL
+apellido VARCHAR(50) NOT NULL
+email  VARCHAR(50) NOT NULL
+usuario VARCHAR(50) NOT NULL
+contrasenia VARCHAR(50) NOT NULL
+fecha-nacimiento DATE NOT NULL
+numero-documento INT NOT NULL
+foto VARCHAR(300)
+};
