82eec14dbff78b4e794c59f6d64ac9b00fd5770c
kundo14 <facundo.tabanera@gmail.com>
2022-06-06 10:51:03 -0300

    Update Producto.js
    mas modelos

--- myApp/database/models/Producto.js
+++ myApp/database/models/Producto.js
@@ -1,1 +1,34 @@
-module.exports=()=>{}
+module.exports=()=>{
+
+    let cols = {
+
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        titulo_producto: {
+            type: DataTypes.STRING
+        },
+        imagen_producto: {
+            type: DataTypes.STRING
+        },
+        descripcion_producto:{
+            type: DataTypes.STRING
+        },
+        created_at: {
+            type: DataTypes.DATE
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER
+        },
+        comentario_id: {
+            type: DataTypes.INTEGER
+        }
+    }
+    let config = {
+        tablename: 'productos',
+    };
+    const Comentario = sequelize.define ("Producto",cols, config);
+    return Producto;
+}
