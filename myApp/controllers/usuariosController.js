b88d3f567294c444757b584d718ba80e3e88c4b8
kundo14 <facundo.tabanera@gmail.com>
2022-06-13 10:18:44 -0300

    Usuarios Controller
    ../

--- myApp/controllers/usuariosController.js
+++ myApp/controllers/usuariosController.js
@@ -1,6 +1,7 @@
 const db =require('../database/models')
 const user = db.Usuario;
 
+
 const usuariosController = {
   index: (req, res) => {
     db.Usuario.findAll({
@@ -15,15 +16,150 @@ const usuariosController = {
       });
   },
   registro: function (req, res) {
-    res.render("registro", {
-      usuario: data.usuario,
-    });
+    if (req.session.usuarioLogueado != undefined){
+      res.redirect("/");
+    }
+    if (req.query.error){
+      res.render('registro', {title: 'Registración', error: true, message:'El nombre de usuario o email ya existe'});
+    }else {
+      res.render('registro', {title:'Registración', error: false, message: 'El nombre de usuario o email ya existe'})
+    }
   },
+  registroCheck: function (req,res){
+    if (req.session.usuarioLogueado != undefined){
+      res.redirect("/");
+    }
+    db.Usuario.findOne({
+      where:{
+        [op.or]:[
+            {email: req.body.email},
+            {nombre: req.body.nombre}
+        ]
+      }
+    })
+    .then(function (usuario) {
+      if(req.body.email == ""){
+          res.render('registro', {title: 'Registración', error: true, message:'El email no puede estar vacío'});
+      } else if (req.body.password == ""){
+          res.render('registro', {title: 'Registración', error: true, message:'La contraseña no puede estar vacía'});
+      } else if (usuario != null){
+          res.render('registro', {title: 'Registración', error: true, message:'El nombre de usuario o el email ya existe. Elija otro.'});
+      } else if(usuario == null){
+          let password = bcrypt.hashSync(req.body.password, 10);
+
+          db.Usuario.create({
+              nombre: req.body.nombre,
+              email: req.body.email,
+              password: password,
+          })
+          .then(function () {
+              res.redirect('/usuario/login');
+          })
+      }else {
+          res.redirect('/usuario/registro?error=true');
+      }
+      
+  })
+  },
+
+  loginForm: function(req,res){
+    if (req.session.usuarioLogueado != undefined) {
+      res.redirect("/");
+    }
+
+  if(req.query.error == 'usuario'){
+    res.render('login', {tittle: 'Login', error: 'usuario' })
+  } else if (req.query.error == 'password'){
+    res.render('login', { tittle: 'Login', error: 'password'})
+  }else{
+    res.render('login', {tittle: 'Login', error: false})
+  }
+  },
+
+
   login: function (req, res) {
-    res.render("login", {
-      usuario: data.usuario,
-    });
+    if (req.session.usuarioLogueado != undefined){
+      res.redirect("/");
+    }
+    db.Usuario.findOne({
+      where: {
+        [op.or]: [
+          {email: req.body.nombre},
+          {nombre: req.body.nombre}
+        ]
+      }
+    })
+    .then(function (usuario) {
+      if(!usuario){
+          res.redirect('/usuario/login?error=usuario');
+      } else if (bcrypt.compareSync(req.body.password, usuario.password) == false ){
+          res.redirect('/usuario/login?error=password');
+      }else{
+          req.session.usuarioLogueado = usuario;
+          if(req.body.recordar != undefined){
+              res.cookie('usuarioLogueadoId', usuario.id, { maxAge: 1000 * 60 * 60 * 24 * 365});
+          }
+          
+          res.redirect('/')
+      }
+  })
   },
+
+  logout: function (req,res) {
+    if (req.session.usuarioLogueado != undefined) {
+        res.redirect("/");
+    }
+    req.session.usuarioLogueado = undefined;
+    res.clearCookie('idUsuario');
+    return res.redirect('/');
+},
+comentarios: function (req, res) {
+  if (req.session.usuarioLogueado == undefined) {
+      res.redirect("/");
+  }
+  let usuario_id = req.session.usuarioLogueado.id;
+  db.Comentario.findAll({
+      where: [
+          {usuario_id: usuario_id}
+      ],
+      order:[
+          ['createdAt', 'DESC']
+      ],
+      include: [  
+          {association: 'producto'},
+          {association: 'usuario'}
+      ]
+  })
+  .then(function (comentarios) {
+      res.render('misComentarios', { comentarios: comentarios, title: 'Mis comentarios' })
+  })
+},
+borrarComentario: function (req,res) {
+  if (req.session.usuarioLogueado == undefined) {
+      res.redirect("/");
+  }
+  let id = req.params.id;
+  db.Comentario.findByPk(id)
+  .then(function (comentario) {
+      res.render('borrarComentario', {comentario: comentario, title: 'Borrar comentario'})
+  })
+},
+ dropComentario: function (req,res) {
+        if (req.session.usuarioLogueado == undefined) {
+            res.redirect("/");
+        }
+        let id = req.params.id;
+        db.Comentario.destroy({
+            where: [
+                { id:id }
+            ]
+        })
+        .then(function (result) {
+            console.log(result);
+            res.redirect('/usuario/comentarios');
+        })
+    },
+
   editarPerfil: function (req, res) {
     res.render("editarPerfil", {
       usuario: data.usuario,
@@ -36,13 +172,7 @@ const usuariosController = {
       comentarios: data.comentarios,
     });
   },
-  //create: (req, res) => {
-  //  return res.render('registro')
-  //}
-  //store: function (req, res) {
-  //  let info = req.body;
-  //  let usuario = 
-  //}
+
 };
 
 module.exports = usuariosController;
